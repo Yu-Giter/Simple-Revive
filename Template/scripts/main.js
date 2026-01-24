745c4bd138c5e00de5343ca683f4346a4a86e5fe
Yu-Gitter <yadomini.7274.strator@gmail.com>
2026-01-24 12:48:39 +0900

    プレイヤーが死亡した時にブロックを置く処理を追加
    これが墓の前身になり，以後，墓ブロック機能や蘇生機能を実装していきます(動作未確認)

--- Template/scripts/main.js
+++ Template/scripts/main.js
@@ -1,3 +1,30 @@
 import * as server from '@minecraft/server';
 
 //ここにスクリプトを記述
+import { world, system } from '@minecraft/server';
+
+world.afterEvents.entityDie.subscribe((event) => {
+    const { deadEntity } = event;
+    if (deadEntity.typeId === "minecraft:player") {
+        const location = deadEntity.location;
+
+        // 端数切捨て
+        const blockPos = {
+            x:Math.floor(location.x),
+            y:Math.floor(location.y),
+            z:Math.floor(location.z)
+        };
+
+        try {
+            // ブロック設置
+            const dimension = deadEntity.dimension;
+            dimension.getBlock(blockPos).setType("minecraft:dirt");
+            
+            // ログ出力
+            console.warn(`Player ${deadEntity.nameTag} died at${blockPos.x}, ${blockPos.y}, ${blockPos.z}. Block placed.`);
+        } catch (error) {
+            console.error("Failed to place block: ", error);
+        }
+          
+    }
+});
