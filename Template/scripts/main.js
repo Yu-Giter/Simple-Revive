c996b51eebd37bbcedf167936ce99d83e978e3c0
Yu-Gitter <yadomini.7274.strator@gmail.com>
2026-02-01 09:45:01 +0900

    Refactor event handling for player interactions
    This is no-code beta

--- Template/scripts/main.js
+++ Template/scripts/main.js
@@ -1,30 +1,73 @@
-import * as server from '@minecraft/server';
+import { world, system, ItemStack, BlockPermutation, Player } from "@minecraft/server";
 
-//ここにスクリプトを記述
-import { world, system } from '@minecraft/server';
+var Plinventory, CountingPLinv, chestpos;
 
-world.afterEvents.entityDie.subscribe((event) => {
-    const { deadEntity } = event;
-    if (deadEntity.typeId === "minecraft:player") {
-        const location = deadEntity.location;
 
-        // 端数切捨て
-        const blockPos = {
-            x:Math.floor(location.x),
-            y:Math.floor(location.y),
-            z:Math.floor(location.z)
-        };
-
-        try {
-            // ブロック設置
-            const dimension = deadEntity.dimension;
-            dimension.getBlock(blockPos).setType("minecraft:dirt");
-            
-            // ログ出力
-            console.warn(`Player ${deadEntity.nameTag} died at${blockPos.x}, ${blockPos.y}, ${blockPos.z}. Block placed.`);
-        } catch (error) {
-            console.error("Failed to place block: ", error);
-        }
-          
+  world.afterEvents.entityHitEntity.subscribe(arg => {
+    if (world.scoreboard.getObjective('money') && arg.damagingEntity?.scoreboardIdentity ? world.scoreboard.getObjective('money')?.getScore(arg.damagingEntity?.scoreboardIdentity) : 0 != undefined) {
+      {
+      const objective = world.scoreboard.getObjective('chestposX');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.x), 10) || 0);
+      }
     }
+      {
+      const objective = world.scoreboard.getObjective('chestposY');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.y), 10) || 0);
+      }
+    }
+      {
+      const objective = world.scoreboard.getObjective('chestposZ');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.z), 10) || 0);
+      }
+    }
+      Plinventory = [(arg.damagingEntity)?.getComponent("inventory")?.container.getItem((arg.damagingEntity)?.selectedSlotIndex)];
+      if (Plinventory == undefined) {
+    } else {
+        if ((arg.damagingEntity)?.getComponent("minecraft:health")?.currentValue <= 0) {
+          chestpos = arg.damagingEntity?.location;
+          world.getDimension('overworld')?.setBlockPermutation({ x: world.scoreboard.getObjective('ChestPosX') && arg.hitEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosX')?.getScore(arg.hitEntity?.scoreboardIdentity) : 0, y: world.scoreboard.getObjective('ChestPosY') && arg.damagingEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosY')?.getScore(arg.damagingEntity?.scoreboardIdentity) : 0, z: world.scoreboard.getObjective('ChestPosZ') && arg.damagingEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosZ')?.getScore(arg.damagingEntity?.scoreboardIdentity) : 0 }, BlockPermutation.resolve('minecraft:chest'));
+          world.getDimension('overworld')?.setBlockPermutation({ x: world.scoreboard.getObjective('ChestPosX') && arg.hitEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosX')?.getScore(arg.hitEntity?.scoreboardIdentity) : 0 + 1, y: world.scoreboard.getObjective('ChestPosY') && arg.damagingEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosY')?.getScore(arg.damagingEntity?.scoreboardIdentity) : 0, z: world.scoreboard.getObjective('ChestPosZ') && arg.damagingEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosZ')?.getScore(arg.damagingEntity?.scoreboardIdentity) : 0 }, BlockPermutation.resolve('minecraft:chest'));
+      }
+    }
+      var CountingPLinv_end = Plinventory.length;
+    var CountingPLinv_inc = 1;
+    if (0 > CountingPLinv_end) {
+      CountingPLinv_inc = -CountingPLinv_inc;
+    }
+    for (CountingPLinv = 0; CountingPLinv_inc >= 0 ? CountingPLinv <= CountingPLinv_end : CountingPLinv >= CountingPLinv_end; CountingPLinv += CountingPLinv_inc) {
+      // 空白
+        arg.hitEntity.runCommand(['/replaceitem block ',world.scoreboard.getObjective('ChestPosX') && arg.hitEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosX')?.getScore(arg.hitEntity?.scoreboardIdentity) : 0,' ',world.scoreboard.getObjective('ChestPosY') && arg.hitEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosY')?.getScore(arg.hitEntity?.scoreboardIdentity) : 0,'　',world.scoreboard.getObjective('ChestPosZ') && arg.hitEntity?.scoreboardIdentity ? world.scoreboard.getObjective('ChestPosZ')?.getScore(arg.hitEntity?.scoreboardIdentity) : 0,'　','slot.container',CountingPLinv,Plinventory[CountingPLinv],' 1'].join(''));
+        }
+  } else {
+      world.scoreboard.addObjective('chestposX', 'chestposX');
+      world.scoreboard.addObjective('chestposY', 'chestposY');
+      world.scoreboard.addObjective('chestposZ', 'chestposZ');
+      {
+      const objective = world.scoreboard.getObjective('chestposX');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.x), 10) || 0);
+      }
+    }
+      {
+      const objective = world.scoreboard.getObjective('chestposY');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.y), 10) || 0);
+      }
+    }
+      {
+      const objective = world.scoreboard.getObjective('chestposZ');
+      const targetEntity = arg.damagingEntity;
+      if (objective && targetEntity?.scoreboardIdentity) {
+        objective.setScore(targetEntity.scoreboardIdentity, parseInt(Math.floor(arg.damagingEntity?.location.z), 10) || 0);
+      }
+    }
+  }
 });
